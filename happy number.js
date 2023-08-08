042ad9b539424bd007ca89b47398cb42961cda99
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-08 23:36:00 +0530

    Create happy number.js

---
+++ happy number.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function (n) {
+    function getNextNumber(num) {
+        let sum = 0;
+        while (num > 0) {
+            const digit = num % 10;
+            sum += digit * digit;
+            num = Math.floor(num / 10);
+        }
+        return sum;
+    }
+
+    let slow = n;
+    let fast = n;
+
+    do {
+        slow = getNextNumber(slow);
+        fast = getNextNumber(getNextNumber(fast));
+    } while (slow !== fast);
+
+    return slow === 1;
+}
