1f92f5411d9ff3291366ce9cab891ccb5f1eae56
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-10 23:22:19 +0530

    Update jewels-and-stones.js

--- jewels-and-stones.js
+++ jewels-and-stones.js
@@ -1,1 +1,21 @@
 
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+    let jewelCount = 0;
+
+    // Create a Set to store the jewel types for faster lookup
+    const jewelSet = new Set(jewels);
+
+    // Count the occurrences of each stone type that is also a jewel
+    for (let i = 0; i < stones.length; i++) {
+        if (jewelSet.has(stones[i])) {
+            jewelCount++;
+        }
+    }
+
+    return jewelCount;
+};
