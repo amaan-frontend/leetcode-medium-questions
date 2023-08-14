d03a00fe9117d9443438068c55eb166f415f85b2
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-14 23:07:44 +0530

    Update unique-binary-search-tree.js

--- unique-binary-search-tree.js
+++ unique-binary-search-tree.js
@@ -1,1 +1,43 @@
+/**
+ * Definition for a binary tree node.
+  * function TreeNode(val, left, right) {
+   *     this.val = (val===undefined ? 0 : val)
+    *     this.left = (left===undefined ? null : left)
+     *     this.right = (right===undefined ? null : right)
+      * }
+       */
+/**
+ * @param {number} n
+  * @return {TreeNode[]}
+   */
+var generateTrees = function (n) {
+    if (n === 0) {
+        return [];
+    }
 
+    return generateTreesHelper(1, n);
+};
+
+function generateTreesHelper(start, end) {
+    const result = [];
+
+    if (start > end) {
+        result.push(null);
+        return result;
+    }
+
+    for (let i = start; i <= end; i++) {
+        const leftTrees = generateTreesHelper(start, i - 1);
+        const rightTrees = generateTreesHelper(i + 1, end);
+
+        for (const leftTree of leftTrees) {
+            for (const rightTree of rightTrees) {
+                const root = new TreeNode(i, leftTree, rightTree);
+                result.push(root);
+            }
+        }
+    }
+
+    return result;
+}
+     
