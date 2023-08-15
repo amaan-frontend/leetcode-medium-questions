51d18d440662e9e0458751de6d075a654de515a5
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-15 22:17:39 +0530

    Update summary-ranges.js

--- summary-ranges.js
+++ summary-ranges.js
@@ -1,1 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function (nums) {
+    const ranges = [];
 
+    for (let i = 0; i < nums.length; i++) {
+        let start = nums[i];
+        while (i < nums.length - 1 && nums[i + 1] === nums[i] + 1) {
+            i++;
+        }
+        let end = nums[i];
+
+        if (start === end) {
+            ranges.push(start.toString());
+        } else {
+            ranges.push(`${start}->${end}`);
+        }
+    }
+
+    return ranges;
+};
