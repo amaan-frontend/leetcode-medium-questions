df55dd40b4db422ed1d1dea700fc39700ea3d416
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-14 23:47:37 +0530

    Update design-hshset.js

--- design-hshset.js
+++ design-hshset.js
@@ -1,1 +1,32 @@
+class MyHashSet {
+    constructor() {
+        this.set = [];
+    }
+
+    add(key) {
+        const index = this.hash(key);
+        if (!this.set[index]) {
+            this.set[index] = [];
+        }
+        if (!this.set[index].includes(key)) {
+            this.set[index].push(key);
+        }
+    }
+
+    remove(key) {
+        const index = this.hash(key);
+        if (this.set[index]) {
+            this.set[index] = this.set[index].filter(k => k !== key);
+        }
+    }
+
+    contains(key) {
+        const index = this.hash(key);
+        return !!this.set[index] && this.set[index].includes(key);
+    }
+
+    hash(key) {
+        return key % 769; // Use a prime number to reduce collisions
+    }
+}
 
