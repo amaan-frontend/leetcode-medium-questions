4f94171c8718c99e89f9fd87d942ba4781e0e668
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-17 23:58:20 +0530

    Update find-first-and-last-position-of-element-in-sorted-array.js

--- find-first-and-last-position-of-element-in-sorted-array.js
+++ find-first-and-last-position-of-element-in-sorted-array.js
@@ -1,1 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
 
+    function binarySearch(nums, target, leftMost) {
+        let left = 0;
+        let right = nums.length - 1;
+        let result = -1;
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+
+            if (nums[mid] === target) {
+                result = mid;
+                if (leftMost) right = mid - 1;
+                else left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    const leftIndex = binarySearch(nums, target, true);
+    const rightIndex = binarySearch(nums, target, false);
+
+    return [leftIndex, rightIndex];
+};
