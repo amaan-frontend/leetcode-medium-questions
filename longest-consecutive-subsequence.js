4d96fbcf04314432ed961fa97aac50e020982ab7
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-19 22:41:35 +0530

    Update longest-consecutive-subsequence.js

--- longest-consecutive-subsequence.js
+++ longest-consecutive-subsequence.js
@@ -1,1 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {
+    const numSet = new Set(nums);
+    let maxLength = 0;
 
+    for (const num of numSet) {
+        if (!numSet.has(num - 1)) { // Only start counting from the smallest element in a potential sequence
+            let currentNum = num;
+            let currentStreak = 1;
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+
+            maxLength = Math.max(maxLength, currentStreak);
+        }
+    }
+
+    return maxLength;
+};
