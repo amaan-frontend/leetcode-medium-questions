b9b12a1d52c76ea8f170e975599b6bbda8575046
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-22 21:49:28 +0530

    Update can-place-flowers.js

--- can-place-flowers.js
+++ can-place-flowers.js
@@ -1,1 +1,14 @@
-
+var canPlaceFlowers = function (flowerbed, n) {
+    let count = 0;
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0) {
+            let prev = i === 0 ? 0 : flowerbed[i - 1];
+            let next = i === flowerbed.length - 1 ? 0 : flowerbed[i + 1];
+            if (prev === 0 && next === 0) {
+                flowerbed[i] = 1;
+                count++;
+            }
+        }
+    }
+    return count >= n;
+};
