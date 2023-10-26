673cddf7d2ea97dc9f92b569a07918c1b6fd3d01
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-26 22:53:54 +0530

    Update linkedlist-cycle.js

--- linkedlist-cycle.js
+++ linkedlist-cycle.js
@@ -3,10 +3,6 @@ function ListNode(val) {
     this.next = null;
 }
 
-/**
- * @param {ListNode} head
-  * @return {boolean}
-   */
 var hasCycle = function (head) {
     if (head === null || head.next === null) {
         return false;
