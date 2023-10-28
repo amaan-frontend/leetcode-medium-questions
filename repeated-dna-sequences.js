4e1d813bf8a7877957026bf3a62c93ea9f3ed09f
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-28 23:33:26 +0530

    Update repeated-dna-sequences.js

--- repeated-dna-sequences.js
+++ repeated-dna-sequences.js
@@ -1,1 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function (s) {
+    const seen = new Set();
+    const repeated = new Set();
+    const result = [];
 
+    for (let i = 0; i <= s.length - 10; i++) {
+        const sequence = s.substring(i, i + 10);
+
+        if (seen.has(sequence)) {
+            repeated.add(sequence);
+        } else {
+            seen.add(sequence);
+        }
+    }
+
+    repeated.forEach(seq => result.push(seq));
+    return result;
+};
