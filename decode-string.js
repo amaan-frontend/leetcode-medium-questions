daec4224ae3eafd02b5651d38d61ad0291628fc4
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-29 23:39:22 +0530

    Update decode-string.js

--- decode-string.js
+++ decode-string.js
@@ -1,1 +1,13 @@
+function decodeString(s) {
+    const stack = [];
+    let currentNum = 0;
+    let currentStr = '';
+
+    for (let char of s) {
+        if (char >= '0' && char <= '9') {
+            currentNum = currentNum * 10 + parseInt(char);
+        } else if (char === '[') {
+            stack.push({ num: currentNum, str: currentStr });
+…    return currentStr;
+}
 
