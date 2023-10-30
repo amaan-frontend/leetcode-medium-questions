df70d9a5186260b7e53614ab98e1cd93b0bb9dcb
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-30 23:56:53 +0530

    Update random-pick-index.js

--- random-pick-index.js
+++ random-pick-index.js
@@ -1,1 +1,23 @@
 
+class Solution {
+    constructor(nums) {
+        this.nums = nums;
+    }
+
+    pick(target) {
+        let count = 0;
+        let result = -1;
+
+        for (let i = 0; i < this.nums.length; i++) {
+            if (this.nums[i] === target) {
+                count++;
+                // With probability 1/count, replace the result with the current index
+                if (Math.floor(Math.random() * count) === 0) {
+                    result = i;
+                }
+            }
+        }
+
+        return result;
+    }
+}
