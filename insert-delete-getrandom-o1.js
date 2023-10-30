08d4bb6dfb8e083f63321a5bb8eab9d41ec31de5
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-30 23:57:25 +0530

    Update insert-delete-getrandom-o1.js

--- insert-delete-getrandom-o1.js
+++ insert-delete-getrandom-o1.js
@@ -1,1 +1,39 @@
+class RandomizedSet {
+    constructor() {
+        this.values = [];
+        this.valToIndex = new Map();
+    }
+
+    insert(val) {
+        if (this.valToIndex.has(val)) {
+            return false;
+        }
+
+        this.values.push(val);
+        this.valToIndex.set(val, this.values.length - 1);
+        return true;
+    }
+
+    remove(val) {
+        if (!this.valToIndex.has(val)) {
+            return false;
+        }
+
+        const indexToRemove = this.valToIndex.get(val);
+        const lastVal = this.values[this.values.length - 1];
+
+        this.values[indexToRemove] = lastVal;
+        this.valToIndex.set(lastVal, indexToRemove);
+
+        this.values.pop();
+        this.valToIndex.delete(val);
+
+        return true;
+    }
+
+    getRandom() {
+        const randomIndex = Math.floor(Math.random() * this.values.length);
+        return this.values[randomIndex];
+    }
+}
 
