834a8fb9f57dbdc07085b7acc95d498cdcad4c76
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-30 23:56:19 +0530

    Update maximum-xor-of-two-numbers-in-an-array.js

--- maximum-xor-of-two-numbers-in-an-array.js
+++ maximum-xor-of-two-numbers-in-an-array.js
@@ -1,1 +1,56 @@
+class TrieNode {
+    constructor() {
+        this.children = new Map();
+    }
+}
 
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(num) {
+        let node = this.root;
+
+        for (let i = 31; i >= 0; i--) {
+            const bit = (num >> i) & 1;
+
+            if (!node.children.has(bit)) {
+                node.children.set(bit, new TrieNode());
+            }
+
+            node = node.children.get(bit);
+        }
+    }
+
+    findMaxXOR(num) {
+        let node = this.root;
+        let result = 0;
+
+        for (let i = 31; i >= 0; i--) {
+            const bit = (num >> i) & 1;
+            const oppositeBit = bit ^ 1;
+
+            if (node.children.has(oppositeBit)) {
+                result |= (1 << i);
+                node = node.children.get(oppositeBit);
+            } else {
+                node = node.children.get(bit);
+            }
+        }
+
+        return result;
+    }
+}
+
+function findMaximumXOR(nums) {
+    const trie = new Trie();
+    let maxXOR = 0;
+
+    for (const num of nums) {
+        trie.insert(num);
+        maxXOR = Math.max(maxXOR, trie.findMaxXOR(num));
+    }
+
+    return maxXOR;
+}
