f69894a089ebe3a9ee8f919933ca51de4d27f71b
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-10-31 12:48:27 +0530

    Update largest divisible subset.js

--- largest divisible subset.js
+++ largest divisible subset.js
@@ -1,1 +1,14 @@
+function largestDivisibleSubset(nums) {
+    nums.sort((a, b) => a - b); // Sort the array in ascending order
+    const n = nums.length;
+    const dp = Array(n).fill(1); // Initialize dp array with 1s
+    let maxLen = 1;
+    let maxIdx = 0;
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] % nums[j] === 0) {
+…
+    return result;
+}
 
