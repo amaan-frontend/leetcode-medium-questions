f1a588d25162dc9c0ddf213128ae695397887b91
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-01 23:07:15 +0530

    Update non-overlapping-intervals.js

--- non-overlapping-intervals.js
+++ non-overlapping-intervals.js
@@ -1,1 +1,19 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function (intervals) {
+    intervals.sort((a, b) => a[1] - b[1]);
+    let end = intervals[0][1];
+    let removeCount = 0;
 
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] < end) {
+            removeCount++;
+        } else {
+            end = intervals[i][1];
+        }
+    }
+
+    return removeCount;
+};
