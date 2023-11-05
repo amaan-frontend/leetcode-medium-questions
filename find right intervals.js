b7f960e7a979f26299bc408e1e4e2510527d1c73
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-05 23:58:28 +0530

    Update find right intervals.js

--- find right intervals.js
+++ find right intervals.js
@@ -1,1 +1,15 @@
+function findRightInterval(intervals) {
+    const n = intervals.length;
+    const result = new Array(n).fill(-1);
+    const sortedIntervals = intervals.map((interval, index) => [interval[0], interval[1], index]);
+
+    sortedIntervals.sort((a, b) => a[0] - b[0]);
+
+    for (let i = 0; i < n; i++) {
+        const targetEnd = intervals[i][1];
+…        }
+    }
+
+    return result;
+}
 
