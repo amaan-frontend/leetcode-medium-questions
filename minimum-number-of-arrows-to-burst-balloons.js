331a3a81382eef60fff9553bba680e19adc77f4f
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-06 23:27:14 +0530

    Update minimum-number-of-arrows-to-burst-balloons.js

--- minimum-number-of-arrows-to-burst-balloons.js
+++ minimum-number-of-arrows-to-burst-balloons.js
@@ -1,1 +1,12 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function (points) {
+    if (points.length === 0) return 0;
 
+    // Sort the balloons based on their end points
+    points.sort((a, b) => a[1] - b[1]);
+
+…    return arrows;
+};
