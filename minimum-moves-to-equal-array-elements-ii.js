b6ca9de7d23c387f399f94a2f5e2b8b78503d09a
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-06 23:30:30 +0530

    Update minimum-moves-to-equal-array-elements-ii.js

--- minimum-moves-to-equal-array-elements-ii.js
+++ minimum-moves-to-equal-array-elements-ii.js
@@ -1,1 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves2 = function (nums) {
+    nums.sort((a, b) => a - b); // Sort the array
+    let median = nums[Math.floor(nums.length / 2)]; // Find the median
 
+    let moves = 0;
+    for (let i = 0; i < nums.length; i++) {
+        moves += Math.abs(nums[i] - median); // Calculate absolute differences
+    }
+
+    return moves;
+};
