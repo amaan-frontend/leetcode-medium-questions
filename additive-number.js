f77189fa8047799417e7d67207de0e1b4684c6ab
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-07 23:39:38 +0530

    Update additive-number.js

--- additive-number.js
+++ additive-number.js
@@ -1,1 +1,16 @@
+function isAdditiveNumber(num) {
+    function dfs(num, index, num1, num2) {
+        if (index === num.length) {
+            return true;
+        }
+
+        let sum = num1 + num2;
+        let sumStr = sum.toString();
+        let len = sumStr.length;
+
+…        }
+    }
+
+    return false;
+}
 
