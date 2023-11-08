6ff2923a952fb4a703c9bb3f227900e5de3aefb9
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-11-08 23:41:25 +0530

    Update number of atoms.js

--- number of atoms.js
+++ number of atoms.js
@@ -1,1 +1,11 @@
+function countOfAtoms(formula) {
+    const stack = [];
+    const counts = new Map();
+    let i = 0;
 
+    function parseNumber() {
+        let num = 0;
+        while (i < formula.length && /^\d$/.test(formula[i])) {
+            num = num * 10 + parseInt(formula[i]);
+            i++;
+…
