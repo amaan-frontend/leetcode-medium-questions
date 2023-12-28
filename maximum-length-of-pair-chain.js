fdc8f05ef902a1d5b0c35eb06d5acdca020c968f
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-12-28 23:51:42 +0530

    Create maximum-length-of-pair-chain.js

---
+++ maximum-length-of-pair-chain.js
@@ -0,0 +1,11 @@
+var findLongestChain = function (pairs) {
+    pairs.sort(([a, b], [c, d]) => b - d);
+    let chain = 1, pair = pairs[0]
+    for (let i = 1; i < pairs.length; i++) {
+        if (pair[1] < pairs[i][0]) {
+            pair = pairs[i]
+            chain++
+        }
+    }
+    return chain
+};
