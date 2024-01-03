222f43feeb09498e74fa94a9044a912614e1f34b
frontender <134868734+amaan-frontend@users.noreply.github.com>
2024-01-03 23:51:06 +0530

    Update reorder-list.js

--- reorder-list.js
+++ reorder-list.js
@@ -1,1 +1,14 @@
-
+var reorderList = function (head) {
+    if (!head || !head.next) return;
+    const nodeArr = [];
+    let cur = head;
+    while (cur) {
+        nodeArr.push(cur);
+        cur = cur.next;
+        nodeArr[nodeArr.length - 1].next = null;
+    }
+    let begin = 0, end = nodeArr.length - 1;
+…            cur.next = nodeArr[begin];
+        }
+    }
+};
