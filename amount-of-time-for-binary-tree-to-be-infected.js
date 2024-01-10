d7f3f31295272798911830a8dfe688ea7631cd3a
frontender <134868734+amaan-frontend@users.noreply.github.com>
2024-01-10 23:33:18 +0530

    Update amount-of-time-for-binary-tree-to-be-infected.js

--- amount-of-time-for-binary-tree-to-be-infected.js
+++ amount-of-time-for-binary-tree-to-be-infected.js
@@ -1,1 +1,18 @@
+var amountOfTime = function (root, start) {
+    let graph = {}
 
+    let inOrder = (node) => {
+        if (!node) return
+
+        inOrder(node.left)
+        if (!graph[node.val]) graph[node.val] = []
+
+        if (node.left) {
+   }
+
+    bfs()
+
+
+    return distance
+
+};
